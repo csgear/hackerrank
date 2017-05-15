b29aaac8d083c3299a5fc410766efac349966d84
csgear <csgear@live.com>
2017-05-15 20:11:45 +0800

    add data structure

---
+++ datastru/arraylr.py
@@ -0,0 +1,12 @@
+
+def rotate(li, x):
+  return li[-x % len(li):] + li[:-x % len(li)]
+
+def lrotate(li, x):
+    return li[x % len(li):] + li[:x % len(li)]
+
+n, d = map(int, input().strip().split())
+l = list(map(int, input().strip().split()))
+
+print(* lrotate(l, d))
+
